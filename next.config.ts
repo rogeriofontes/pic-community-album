c4175c572ca74168ee6cbf50e2dd59fdf4efe93b
Rogerio Fontes <fontestz@gmail.com>
2025-07-10 09:16:03 -0300

    Corrige URL da API e Cors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'take-congress-pics-imagebucket-k8vj2yrrwfw0.s3.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
